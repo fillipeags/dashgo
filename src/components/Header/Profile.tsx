20c3c6c80dbd2ffb223b12ca482562eaebd89e8b
Fillipe <fillipe484@gmail.com>
2021-05-13 13:29:30 -0300

    header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,18 @@
 import { Flex, Box, Text, Avatar} from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({showProfileData  = true } : ProfileProps){
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Fillipe Augusto</Text>
-        <Text color="gray.300" fontSize="small" >fillipe484@gmail.com</Text>
-      </Box>
-
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Fillipe Augusto</Text>
+          <Text color="gray.300" fontSize="small" >fillipe484@gmail.com</Text>
+        </Box>
+      )}
       <Avatar size="md" name="Fillipe Augusto" src="https://github.com/fillipeags.png"/>
     </Flex>
   );
